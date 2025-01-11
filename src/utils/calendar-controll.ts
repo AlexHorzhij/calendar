4b7b3f7adfa3fecce5c243b11c0c36f905af7e55
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2025-01-11 17:45:49 +0200

    deploy git-hub

--- src/utils/calendar-controll.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const useCalendar = (month: number, year: number) => {
-  const [currentYear, setCurrentYear] = useState<number>(
-    year || new Date().getFullYear()
-  );
-};
